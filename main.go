a482ae43ac6c1517cf8a80604fa118e5ec966ae8
douglastse <35443592+douglastsehc@users.noreply.github.com>
2019-03-17 13:45:21 +0800

    upload my code

--- main.go
+++ main.go
@@ -3,14 +3,51 @@ package main
 import (
 	"context"
 	"fmt"
+	"sort"
 
 	"github.com/coreos/go-semver/semver"
 	"github.com/google/go-github/github"
 )
 
+type Versions []*semver.Version
+
+func (s Versions) Len() int {
+	return len(s)
+}
+
+func (s Versions) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s Versions) Less(i, j int) bool {
+	return s[j].LessThan(*s[i])
+}
+
+// Sort sorts the given slice of Version
+func Sort(versions []*semver.Version) {
+	sort.Sort(Versions(versions))
+}
+
 // LatestVersions returns a sorted slice with the highest version as its first element and the highest version of the smaller minor versions in a descending order
 func LatestVersions(releases []*semver.Version, minVersion *semver.Version) []*semver.Version {
 	var versionSlice []*semver.Version
+
+	//semver.Sort(releases)
+	sort.Sort(Versions(releases))
+	//fmt.Printf("%s \n", releases)
+	for i := range releases {
+		var release = releases[i]
+		// make sure the version is later than minVersion and without any prerelease
+		if release.PreRelease != "" || release.Compare(*minVersion) < 0 {
+			continue
+		}
+
+		//fmt.Printf("Every version Version: %s   Major: %d    Minor: %d Patch: %d \n\n", release, release.Major, release.Minor, release.Patch)
+		if len(versionSlice) == 0 || versionSlice[len(versionSlice)-1].Major != release.Major || versionSlice[len(versionSlice)-1].Minor != release.Minor {
+			versionSlice = append(versionSlice, release)
+		}
+	}
+	//fmt.Printf("%s\n", versionSlice)
 	// This is just an example structure of the code, if you implement this interface, the test cases in main_test.go are very easy to run
 	return versionSlice
 }
@@ -26,7 +63,8 @@ func main() {
 	opt := &github.ListOptions{PerPage: 10}
 	releases, _, err := client.Repositories.ListReleases(ctx, "kubernetes", "kubernetes", opt)
 	if err != nil {
-		panic(err) // is this really a good way?
+		//panic(err) // is this really a good way?
+		fmt.Printf("\n%s  \n\n\n", err.Error())
 	}
 	minVersion := semver.New("1.8.0")
 	allReleases := make([]*semver.Version, len(releases))
@@ -38,6 +76,5 @@ func main() {
 		allReleases[i] = semver.New(versionString)
 	}
 	versionSlice := LatestVersions(allReleases, minVersion)
-
 	fmt.Printf("latest versions of kubernetes/kubernetes: %s", versionSlice)
 }
--- main_test.go
+++ main_test.go
@@ -49,6 +49,16 @@ func TestLatestVersions(t *testing.T) {
 			minVersion:     semver.New("2.2.1"),
 		},
 		// Implement more relevant test cases here, if you can think of any
+		{
+			versionSlice:   []string{"2.2.1", "2.2.1", "2.2.1", "2.2.1", "2.2.1", "2.2.0", "2.2.0"},
+			expectedResult: []string{"2.2.1"},
+			minVersion:     semver.New("2.2.1"),
+		},
+		{
+			versionSlice:   []string{"2.2.1", "2.2.1", "2.2.1", "2.2.1", "2.2.1", "2.2.0", "2.2.0"},
+			expectedResult: []string{},
+			minVersion:     semver.New("2.2.2"),
+		},
 	}
 
 	test := func(versionData []string, expectedResult []string, minVersion *semver.Version) {
